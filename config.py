fe01adebda33c7c5f3854f183dd5a8081d07173d
brian-w-projects <brian.weinfeld@gmail.com>
2017-07-05 10:14:27 -0400

    heroku test

--- config.py
+++ config.py
@@ -17,9 +17,12 @@ class DevelopmentConfig(Config):
 
 class DeploymentConfig(Config):
     SECRET_KEY = os.environ['SECRET_KEY']
-    REDIS_URL = os.environ['REDIS_URL']
-    CELERY_BROKER_URL = os.environ['REDIS_URL']
-    CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
+    # REDIS_URL = os.environ['REDIS_URL']
+    # CELERY_BROKER_URL = os.environ['REDIS_URL']
+    # CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
+    REDIS_URL = 'redis://:@localhost:6379/0'
+    CELERY_BROKER_URL = 'redis://:@localhost:6379/0'
+    CELERY_RESULT_BACKEND = 'redis://:@localhost:6379/0'
 
 
 config = {
