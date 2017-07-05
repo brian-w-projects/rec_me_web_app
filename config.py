a2e1e47c4e6032a1b370c4bab9b3fc9481066b88
brian-w-projects <brian.weinfeld@gmail.com>
2017-07-05 10:28:47 -0400

    virtualenv update

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SECRET_KEY = 'asdlfasdflkjsdf'
-    REDIS_URL = 'redis://:@localhost:6379/0'
-    CELERY_BROKER_URL = 'redis://:@localhost:6379/0'
-    CELERY_RESULT_BACKEND = 'redis://:@localhost:6379/0'
+    REDIS_URL = 'redis://:@localhost:6379/1'
+    CELERY_BROKER_URL = 'redis://:@localhost:6379/1'
+    CELERY_RESULT_BACKEND = 'redis://:@localhost:6379/1'
 
 
 class DeploymentConfig(Config):
